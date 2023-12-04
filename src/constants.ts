d02f4eb028cd8be4372a99b28d63ad7008a98a25
= <=>
2023-12-05 00:14:58 +0200

    Update BASE_URLS in constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -6,6 +6,6 @@ export const PROTECTED_ROUTES = [
 ];
 
 export const BASE_URLS = {
-  authentication: process.env.AUTH_SERVER || 'http://localhost:4000',
-  books: process.env.BOOKS_SERVER || 'http://localhost:4001',
+  authentication: process.env.NEXT_PUBLIC_AUTH_SERVER || 'http://localhost:4000',
+  books: process.env.NEXT_PUBLIC_BOOKS_SERVER || 'http://localhost:4001',
 };
