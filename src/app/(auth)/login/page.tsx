8732c10ae2a78de412ca7d8862f8a3bf3b7603ee
MustafaWael <mustafa.wael.dev@gmail.com>
2023-12-16 11:28:47 +0200

    Add metadata for login page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,5 +1,15 @@
 import LoginForm from '@/components/forms/loginForm';
+import { Metadata } from 'next';
 import Link from 'next/link';
+
+export const metadata: Metadata = {
+  title: 'Bookstore | Login',
+  description: 'Login to your Bookstore account',
+  openGraph: {
+    title: 'Bookstore | Login',
+    description: 'Login to your Bookstore account',
+  },
+};
 
 export default function LoginPage() {
   return (
